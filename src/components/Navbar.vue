18d2b982021fca4920e3d530de535085eb0cbb85
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-09-24 18:01:46 +0200

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="navbar">
+    <img src="../assets/images/logo.svg" alt="">
+   
+  </nav>
+</template>
+
+
+<style scoped>
+.navbar {
+    padding: 50px 20px ;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
